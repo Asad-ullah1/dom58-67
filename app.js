e64216e7bcba6f3b974b666d4643a1cca2a8b56c
Asad <52581107+Asad-ullah1@users.noreply.github.com>
2022-06-17 13:50:17 +0500

    commt

---
+++ app.js
@@ -0,0 +1,57 @@
+// question 1
+
+// var ass = document.getElementById('form-content');
+// // question ii
+// console.log(ass.children);
+
+//question (iii)
+
+
+// var rander = document.getElementsByClassName("render")
+// for (i = 0; i < rander.length; i++) {
+
+//     document.write(rander[i].innerHTML + "<br>");
+
+// }
+// //question (iv)
+
+
+// var firstName = document.getElementById("first-name");
+// firstName.value = "asad";
+
+// //question (v)
+
+// var lastName = document.getElementById("last-name");
+// lastName.value = "Khan"
+
+// var email = document.getElementById("email");
+// email.value = "ak659277@gmail.com";
+
+
+///q1
+// var main = document.getElementById("form-content");
+// document.write("Node Type of form content " + main.nodeType);
+
+//q2
+// var last = document.childNodes[1].childNodes[1].childNodes[0].childNodes[1];
+// console.log(last)
+// document.write("<br> node type of last name " + last);
+//q3
+// var lastChange = document.childNodes[1].childNodes[1].childNodes[0].childNodes[2].childNodes[1].innerHTML = "Last Name:     Khan";
+// document.write("<br>"+lastChange)
+//q4
+
+// var frst = document.getElementById("main-content")
+// var frstChild = frst.firstChild; 
+// var lstChild = frst.lastChild
+// console.log(frstChild, lstChild);
+
+//q5
+// var lastid = document.getElementById("lastName");
+// var frstsb = lastid.nextSibling;
+// var prsb = lastid.previousSibling;
+// console.log(frstsb,prsb);
+ //q 6
+//  var emml = getElementById("email");
+//  console.log(emml.nodeType);
+//  console.log(emml.parentNode)
